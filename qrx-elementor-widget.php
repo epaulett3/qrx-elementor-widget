ba96824ea0d6b0c5071ce12d9311d6fcd0c517ce
epaulett3 <jc.paul.pospos@gmail.com>
2022-12-29 14:43:22 +0800

    added comments

--- qrx-elementor-widget.php
+++ qrx-elementor-widget.php
@@ -29,9 +29,12 @@ define('QRXEW_TXT_DOMAIN', 'qrx-elementor-widget');
  */
 function register_qrxelementor_widget( $widgets_manager ) {
 
+    // require widget PHP files
 	require_once( __DIR__ . '/widgets/prodcat-list-widget.php' );
 	require_once( __DIR__ . '/widgets/imagebox-with-button-widget.php' );
 
+    // register widgets
+    
     // custom widgets
     $widgets_manager->register( new \Widget_Image_Box_With_Button() ); 
 
