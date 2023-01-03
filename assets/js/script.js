406f94e021ff3ec9d3ea628a6bd2ff6fe2d44f16
epaulett3 <jc.paul.pospos@gmail.com>
2023-01-03 12:07:55 +0800

    Updated the JS scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
-jQuery(document).ready(function($){
+(function ($) {
+    // SCRIPT FOR QRX NAV MENU WIDGET
+    const QRxNavMenu = function ($scope, $) {
+        // js code where $scope is your element from widget above
+        // console.log($scope.find('.elementor-menu-toggle'));
+        $scope.find('.elementor-menu-toggle').on('click', function(){
+            // console.log('testing on elementor edit');
+            if($(this).hasClass('elementor-active')) {
+                $(this).removeClass('elementor-active');
+            }else{
+                $(this).addClass('elementor-active');
+            }
+        });
+        
+    };
 
-    
-    // FOR QRX NAV MENU SCRIPT
-    $('.elementor-widget-qrx-nav-menu .elementor-menu-toggle').on('click', function(){
-        console.log('testing on elementor edit');
-        if($(this).hasClass('elementor-active')) {
-            $(this).removeClass('elementor-active');
-        }else{
-            $(this).addClass('elementor-active');
-        }
-    });
-});
+    // initialize custom function to elementor frontend
+    $(window).on('elementor/frontend/init', function () {
+        elementorFrontend.hooks.addAction('frontend/element_ready/qrx-nav-menu.default', QRxNavMenu);
+    })
+})(jQuery);
