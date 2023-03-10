6dd8f9a030cef69cd42e1a71c3145296c892659d
epaulett3 <jc.paul.pospos@gmail.com>
2023-03-10 18:17:47 +0800

    Updated plugin version to 1.0.6

--- qrx-elementor-widget.php
+++ qrx-elementor-widget.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: QRx Elementor Widget
 Description: A plugin that addes custom Elementor Widget
-Version: 1.0.5
+Version: 1.0.6
 Author: Jessie Paul (Kaival Marketing Services)
 Author URI: kaivalmarketingservices.com
 
